65d4311cbcaf1df71f8dea386cc17e3f51005e3e
21Maryori <cosimaryo@gmail.com>
2022-04-27 14:03:33 -0500

    first commit

---
+++ src/main/java/grandesalmacenes/Puerta.java
@@ -0,0 +1,27 @@
+package grandesalmacenes;
+
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Puerta {
+        boolean ocupada;
+
+        Puerta(){
+                this.ocupada=false;
+
+        }
+        public boolean estaOcupada(){
+                return this.ocupada;
+        }
+        public synchronized void liberarPuerta(){
+                this.ocupada=false;
+        }
+        public synchronized boolean intentarEntrar(){
+                if (this.ocupada) return false;
+                
+                this.ocupada=true;
+                return true;
+        }
+}
